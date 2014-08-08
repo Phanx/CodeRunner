ada51c5635a0f2d35e7f1389f5e07cfe2a3b27ff
Phanx <addons@phanx.net>
2014-08-08 06:37:51 +0000

    - Add files, attributes

---
+++ Addon.lua
@@ -0,0 +1,294 @@
+--[[--------------------------------------------------------------------
+	Code Runner
+	Edit and run Lua code in-game.
+	Written by Phanx <addons@phanx.net>
+	Inspired by Tekkub's addon tekPad.
+----------------------------------------------------------------------]]
+
+local ADDON = ...
+
+local db, SELECTION
+local SCROLL_FRAME_HEIGHT, LINE_HEIGHT, offset, maxOffset = 332, 12, 0, 0
+
+local L_GO = "Go!"
+if GetLocale() == "deDE" then
+	L_GO = "Los!"
+elseif GetLocale():match("es") then
+	L_GO = "Vamos!"
+end
+
+local LSM = LibStub("LibSharedMedia-3.0")
+LSM:Register("font", "Andale Mono", "Interface\\AddOns\\"..ADDON.."\\Fonts\\AndaleMono.ttf")
+LSM:Register("font", "Consolas", "Interface\\AddOns\\"..ADDON.."\\Fonts\\Consolas.ttf")
+LSM:Register("font", "Cousine", "Interface\\AddOns\\"..ADDON.."\\Fonts\\Cousine.ttf")
+LSM:Register("font", "Fira Mono", "Interface\\AddOns\\"..ADDON.."\\Fonts\\FiraMono.otf")
+LSM:Register("font", "Monaco", "Interface\\AddOns\\"..ADDON.."\\Fonts\\Monaco.ttf")
+LSM:Register("font", "PT Mono", "Interface\\AddOns\\"..ADDON.."\\Fonts\\PTMono.ttf")
+LSM:Register("font", "Source Code", "Interface\\AddOns\\"..ADDON.."\\Fonts\\SourceCodePro.otf")
+
+local f = CreateFrame("Frame", "CodeRunner", UIParent, "ButtonFrameTemplate")
+f:SetPoint("TOPLEFT", 16, -116)
+f:SetWidth(800)
+f:SetHeight(425)
+f:EnableMouse(true)
+f:SetMovable(true)
+f:SetClampedToScreen(true)
+f:Hide()
+
+f:SetAttribute("UIPanelLayout-defined", true)
+f:SetAttribute("UIPanelLayout-enabled", true)
+f:SetAttribute("UIPanelLayout-area", "doublewide")
+f:SetAttribute("UIPanelLayout-pushable", 3)
+f:SetAttribute("UIPanelLayout-whileDead", true)
+tinsert(UISpecialFrames, ADDON)
+
+f.TitleText:SetText(ADDON)
+SetPortraitToTexture(f.portrait, "Interface\\AddOns\\" .. ADDON .. "\\Portrait")
+
+------------------------------------------------------------------------
+
+local drag = CreateFrame("Button", nil, f)
+drag:SetPoint("TOPLEFT", CodeRunnerTitleBg, 40, 0)
+drag:SetPoint("BOTTOMRIGHT", CodeRunnerTitleBg)
+f.DragButton = drag
+
+drag:SetScript("OnEnter", function(self)
+	GameTooltip:SetOwner(self, "ANCHOR_TOP", 0, 10)
+	GameTooltip:SetText("Drag to move this frame.\nRight-click to reset its position.")
+end)
+drag:SetScript("OnLeave", GameTooltip_Hide)
+
+drag:RegisterForDrag("LeftButton")
+drag:SetScript("OnDragStart", function(self)
+	f:StartMoving()
+	if GameTooltip:IsOwned(self) then
+		GameTooltip:Hide()
+	end
+end)
+drag:SetScript("OnDragStop", function(self)
+	f:StopMovingOrSizing()
+	f.userPlaced = true
+	if self:IsMouseOver() then
+		self:GetScript("OnEnter")(self)
+	end
+end)
+drag:SetScript("OnHide", function(self)
+	f:StopMovingOrSizing()
+end)
+
+drag:RegisterForClicks("RightButtonUp")
+drag:SetScript("OnClick", function(self, button)
+	if button == "RightButton" then
+		f.userPlaced = false
+		f:Hide()
+		ShowUIPanel(f)
+	end
+end)
+
+------------------------------------------------------------------------
+
+local scrollFrame = CreateFrame("ScrollFrame", "$parentScrollFrame", f)
+scrollFrame:SetPoint("TOPLEFT", 8, -64)
+scrollFrame:SetPoint("BOTTOMRIGHT", -32, 29)
+SCROLL_FRAME_HEIGHT = scrollFrame:GetHeight()
+f.ScrollFrame = scrollFrame
+
+local scrollBar = CreateFrame("Slider", "$parentScrollBar", scrollFrame, "UIPanelScrollBarTemplate")
+scrollBar:SetPoint("TOPLEFT", scrollFrame, "TOPRIGHT", 6, -16)
+scrollBar:SetPoint("BOTTOMLEFT", scrollFrame, "BOTTOMRIGHT", 6, 16)
+scrollBar:SetScript("OnValueChanged", nil) -- remove default
+scrollBar:SetMinMaxValues(0, 1)
+scrollFrame.ScrollBar = scrollBar
+
+local scrollBarBG = scrollBar:CreateTexture(nil, "BACKGROUND")
+scrollBarBG:SetAllPoints(true)
+scrollBarBG:SetTexture(0,0,0,0.5)
+
+------------------------------------------------------------------------
+
+local focus
+
+local editBox = CreateFrame("EditBox", nil, scrollFrame)
+scrollFrame:SetScrollChild(editBox)
+editBox:SetPoint("TOP")
+editBox:SetPoint("LEFT")
+editBox:SetPoint("RIGHT")
+editBox:SetHeight(1000)
+editBox:SetFontObject(GameFontHighlight)
+editBox:SetTextInsets(5,5,5,5)
+editBox:SetAutoFocus(false)
+editBox:SetMultiLine(true)
+f.EditBox = editBox
+
+editBox:SetScript("OnEscapePressed", editBox.ClearFocus)
+editBox:SetScript("OnTabPressed", function(self) self:Insert("    ") end)
+editBox:SetScript("OnEditFocusGained", function(self) focus:Hide() end)
+editBox:SetScript("OnEditFocusLost", function(self)
+	db[SELECTION] = self:GetText()
+	focus:Show()
+end)
+editBox:SetScript("OnShow", function(self)
+	f.TitleText:SetFormattedText("%s - %s", ADDON, SELECTION or UNKNOWN)
+	self:SetText(db[SELECTION] or "")
+	self:SetFocus()
+end)
+
+local editBG = editBox:CreateTexture(nil, "BACKGROUND")
+editBG:SetAllPoints(true)
+editBG:SetTexture(0,1,0,0.1)
+
+focus = CreateFrame("Button", nil, scrollFrame)
+focus:SetAllPoints(true)
+focus:SetScript("OnClick", function(self) editBox:SetFocus() end)
+f.Focus = focus
+
+------------------------------------------------------------------------
+
+local function ScrollTo(v)
+	offset = max(min(v, 0), maxOffset)
+	scrollFrame:SetVerticalScroll(-offset)
+	editBox:SetPoint("TOP", 0, offset)
+
+	local perc = offset / maxOffset
+	scrollBar:SetValue(perc)
+	scrollBar.ScrollUpButton:SetEnabled(perc > 0.01)
+	scrollBar.ScrollDownButton:SetEnabled(perc < 0.99)
+end
+
+editBox:SetScript("OnCursorChanged", function(self, x, y, width, height)
+	LINE_HEIGHT = height
+	if offset < y then
+		ScrollTo(y)
+	elseif floor(offset - SCROLL_FRAME_HEIGHT + height * 2) > y then
+		local v = y + SCROLL_FRAME_HEIGHT - height * 2
+		maxOffset = min(maxOffset, v)
+		ScrollTo(v)
+	end
+end)
+
+scrollFrame:UpdateScrollChildRect()
+
+scrollFrame:EnableMouseWheel(true)
+scrollFrame:SetScript("OnMouseWheel", function(self, delta)
+	ScrollTo(offset + delta * LINE_HEIGHT * 3)
+end)
+
+scrollBar:EnableMouseWheel(true)
+scrollBar:SetScript("OnMouseWheel", function(self, delta)
+	ScrollTo(offset + delta * LINE_HEIGHT * 3)
+end)
+
+scrollBar.ScrollUpButton:SetScript("OnClick", function()
+	ScrollTo(offset + 1 * LINE_HEIGHT * 3)
+end)
+scrollBar.ScrollDownButton:SetScript("OnClick", function()
+	ScrollTo(offset - 1 * LINE_HEIGHT * 3)
+end)
+
+-- #TODO fix scrollbar with small editbox heights
+
+------------------------------------------------------------------------
+
+local runButton = CreateFrame("Button", "$parentRunButton", f, "MagicButtonTemplate")
+runButton:SetPoint("BOTTOMRIGHT", -7, 5)
+runButton:SetText("Go!") -- TODO: localize
+runButton.RightSeparator:Hide()
+f.RunButton = runButton
+
+runButton:SetScript("OnClick", function() RunScript(editBox:GetText()) end)
+
+------------------------------------------------------------------------
+
+local reloadButton = CreateFrame("Button", "$parentReloadButton", f, "MagicButtonTemplate")
+reloadButton:SetPoint("BOTTOMLEFT", 7, 5)
+reloadButton:SetText("Reload UI") -- TODO: localize
+reloadButton.LeftSeparator:Hide()
+f.ReloadButton = reloadButton
+
+reloadButton:SetScript("OnClick", ReloadUI)
+
+------------------------------------------------------------------------
+
+local font = LibStub("PhanxConfig-ScrollingDropdown"):New(f, "Font", nil, LSM:List("font"))
+font:SetPoint("TOPRIGHT", -10, -15)
+font:SetWidth(200)
+
+font.labelText:ClearAllPoints()
+font.labelText:SetPoint("BOTTOMRIGHT", font, "BOTTOMLEFT", -5, 5)
+
+function font:Callback(value)
+	local file = LSM:Fetch("font", value)
+	local _, size, flag = self.valueText:GetFont()
+	self.valueText:SetFont(file, size, flag)
+
+	CodeRunnerFont = value
+	editBox:SetFont(file, 17, "")
+end
+
+function font:ListButtonCallback(button, value, selected)
+	if button:IsShown() then
+		button:GetFontString():SetFont(LSM:Fetch("font", value), UIDROPDOWNMENU_DEFAULT_TEXT_HEIGHT)
+	end
+end
+
+font.__SetValue = font.SetValue
+function font:SetValue(value)
+	self.valueText:SetFont(LSM:Fetch("font", value), 17, "")
+	self:__SetValue(value)
+end
+
+------------------------------------------------------------------------
+
+SLASH_CODERUNNER1 = "/cr"
+SLASH_CODERUNNER2 = "/code"
+SLASH_CODERUNNER3 = "/coderunner"
+SlashCmdList.CODERUNNER = function()
+	if f.userPlaced then
+		f:SetShown(not f:IsShown())
+	elseif f:IsShown() then
+		HideUIPanel(f)
+	else
+		ShowUIPanel(f)
+	end
+end
+
+------------------------------------------------------------------------
+
+local LDB = LibStub and LibStub("LibDataBroker-1.1", true)
+if LDB then
+	LDB:NewDataObject(ADDON, {
+		type = "launcher",
+		icon = "Interface\\AddOns\\"..ADDON.."\\Icon",
+		OnClick = SlashCmdList.CODERUNNER,
+	})
+end
+
+------------------------------------------------------------------------
+
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, event, addon)
+	if event == "PLAYER_LOGOUT" then
+		for k, v in pairs(db) do
+			if v == "" then
+				db[k] = nil
+			end
+		end
+	elseif addon == ADDON then
+		self:UnregisterEvent(event)
+		self:RegisterEvent("PLAYER_LOGOUT")
+
+		db = CodeRunnerDB or {}
+		CodeRunnerDB = db
+
+		SELECTION = CodeRunnerSelection or UnitName("player")
+		CodeRunnerSelection = SELECTION
+		
+		CodeRunnerFont = CodeRunnerFont or "Consolas"
+		font:SetValue(CodeRunnerFont)
+		editBox:SetFont(LSM:Fetch("font", CodeRunnerFont), 17, "")
+
+		if not db[SELECTION] then
+			db[SELECTION] = ""
+		end
+	end
+end)
