81dac10fcb8a3469f580e53b23a27953b404948d
Phanx <addons@phanx.net>
2014-12-22 03:43:27 -0800

    Delay frame setup to first showing

--- Addon.lua
+++ Addon.lua
@@ -83,216 +83,226 @@ SetPortraitToTexture(f.portrait, "Interface\\AddOns\\" .. ADDON .. "\\Portrait")
 
 ------------------------------------------------------------------------
 
-local drag = CreateFrame("Button", nil, f)
-drag:SetPoint("TOPLEFT", CodeRunnerTitleBg, 40, 0)
-drag:SetPoint("BOTTOMRIGHT", CodeRunnerTitleBg)
-f.DragButton = drag
+f:SetScript("OnShow", function(f)
+	f:SetScript("OnShow", nil)
 
-drag:SetScript("OnEnter", function(self)
-	GameTooltip:SetOwner(self, "ANCHOR_TOP", 0, 10)
-	GameTooltip:SetText("Drag to move this frame.\nRight-click to reset its position.")
-end)
-drag:SetScript("OnLeave", GameTooltip_Hide)
+	local drag = CreateFrame("Button", nil, f)
+	drag:SetPoint("TOPLEFT", CodeRunnerTitleBg, 40, 0)
+	drag:SetPoint("BOTTOMRIGHT", CodeRunnerTitleBg)
+	f.DragButton = drag
 
-drag:RegisterForDrag("LeftButton")
-drag:SetScript("OnDragStart", function(self)
-	f:StartMoving()
-	if GameTooltip:IsOwned(self) then
-		GameTooltip:Hide()
-	end
-end)
-drag:SetScript("OnDragStop", function(self)
-	f:StopMovingOrSizing()
-	f.userPlaced = true
-	if self:IsMouseOver() then
-		self:GetScript("OnEnter")(self)
-	end
-end)
-drag:SetScript("OnHide", function(self)
-	f:StopMovingOrSizing()
-end)
+	drag:SetScript("OnEnter", function(self)
+		GameTooltip:SetOwner(self, "ANCHOR_TOP", 0, 10)
+		GameTooltip:SetText("Drag to move this frame.\nRight-click to reset its position.")
+	end)
+	drag:SetScript("OnLeave", GameTooltip_Hide)
 
-drag:RegisterForClicks("RightButtonUp")
-drag:SetScript("OnClick", function(self, button)
-	if button == "RightButton" then
-		f.userPlaced = false
-		f:Hide()
-		ShowUIPanel(f)
-	end
-end)
-
-------------------------------------------------------------------------
-
-local scrollFrame = CreateFrame("ScrollFrame", "$parentScrollFrame", f)
-scrollFrame:SetPoint("TOPLEFT", 8, -64)
-scrollFrame:SetPoint("BOTTOMRIGHT", -32, 29)
-SCROLL_FRAME_HEIGHT = scrollFrame:GetHeight()
-f.ScrollFrame = scrollFrame
-
-local scrollBar = CreateFrame("Slider", "$parentScrollBar", scrollFrame, "UIPanelScrollBarTemplate")
-scrollBar:SetPoint("TOPLEFT", scrollFrame, "TOPRIGHT", 6, -16)
-scrollBar:SetPoint("BOTTOMLEFT", scrollFrame, "BOTTOMRIGHT", 6, 16)
-scrollBar:SetScript("OnValueChanged", nil) -- remove default
-scrollBar:SetMinMaxValues(0, 1)
-scrollFrame.ScrollBar = scrollBar
-
-local scrollBarBG = scrollBar:CreateTexture(nil, "BACKGROUND")
-scrollBarBG:SetAllPoints(true)
-scrollBarBG:SetTexture(0,0,0,0.5)
-
-------------------------------------------------------------------------
-
-local focus
-
-local editBox = CreateFrame("EditBox", nil, scrollFrame)
-scrollFrame:SetScrollChild(editBox)
-editBox:SetPoint("TOP")
-editBox:SetPoint("LEFT")
-editBox:SetPoint("RIGHT")
-editBox:SetHeight(1000)
-editBox:SetFontObject(GameFontHighlight)
-editBox:SetTextColor(FONT_R, FONT_G, FONT_B)
-editBox:SetTextInsets(5,5,5,5)
-editBox:SetAutoFocus(false)
-editBox:SetMultiLine(true)
-f.EditBox = editBox
-
-editBox:SetScript("OnEscapePressed", editBox.ClearFocus)
-editBox:SetScript("OnTabPressed", function(self) self:Insert("   ") end)
-editBox:SetScript("OnEditFocusGained", function(self) focus:Hide() end)
-editBox:SetScript("OnEditFocusLost", function(self)
-	db[SELECTION] = self:GetText()
-	focus:Show()
-end)
-editBox:SetScript("OnShow", function(self)
-	f.TitleText:SetFormattedText("%s - %s", ADDON, SELECTION or UNKNOWN)
-	self:SetText(db[SELECTION] or "")
-	self:SetFocus()
-end)
---[[
-local editBG = editBox:CreateTexture(nil, "BACKGROUND")
-editBG:SetAllPoints(true)
-editBG:SetTexture(0,1,0,0.1)
-]]
-focus = CreateFrame("Button", nil, scrollFrame)
-focus:SetAllPoints(true)
-focus:SetScript("OnClick", function(self) editBox:SetFocus() end)
-f.Focus = focus
-
-------------------------------------------------------------------------
-
-local function ScrollTo(v)
-	offset = max(min(v, 0), maxOffset)
-	scrollFrame:SetVerticalScroll(-offset)
-	editBox:SetPoint("TOP", 0, offset)
-
-	local perc = maxOffset == 0 and 0 or offset / maxOffset
-	scrollBar:SetValue(perc)
-	scrollBar.ScrollUpButton:SetEnabled(perc > 0.01)
-	scrollBar.ScrollDownButton:SetEnabled(perc < 0.99)
-end
-
-editBox:SetScript("OnCursorChanged", function(self, x, y, width, height)
-	LINE_HEIGHT = height
-	if offset < y then
-		ScrollTo(y)
-	elseif floor(offset - SCROLL_FRAME_HEIGHT + height * 2) > y then
-		local v = y + SCROLL_FRAME_HEIGHT - height * 2
-		maxOffset = min(maxOffset, v)
-		ScrollTo(v)
-	end
-end)
-
-scrollFrame:UpdateScrollChildRect()
-
-scrollFrame:EnableMouseWheel(true)
-scrollFrame:SetScript("OnMouseWheel", function(self, delta)
-	ScrollTo(offset + delta * LINE_HEIGHT * 3)
-end)
-
-scrollBar:EnableMouseWheel(true)
-scrollBar:SetScript("OnMouseWheel", function(self, delta)
-	ScrollTo(offset + delta * LINE_HEIGHT * 3)
-end)
-
-scrollBar.ScrollUpButton:SetScript("OnClick", function()
-	ScrollTo(offset + 1 * LINE_HEIGHT * 3)
-end)
-scrollBar.ScrollDownButton:SetScript("OnClick", function()
-	ScrollTo(offset - 1 * LINE_HEIGHT * 3)
-end)
-
--- #TODO fix scrollbar with small editbox heights
-
-------------------------------------------------------------------------
-
-local runButton = CreateFrame("Button", "$parentRunButton", f, "MagicButtonTemplate")
-runButton:SetPoint("BOTTOMRIGHT", -7, 5)
-runButton:SetText(L.RUN)
-runButton.RightSeparator:Hide()
-f.RunButton = runButton
-
-runButton:SetScript("OnClick", function()
-	RunScript(editBox:GetText())
-end)
-
-------------------------------------------------------------------------
-
-local cancelButton = CreateFrame("Button", "$parentRevertButton", f, "MagicButtonTemplate")
-cancelButton:SetPoint("RIGHT", runButton, "LEFT")
-cancelButton:SetText(L.REVERT)
-cancelButton.RightSeparator:Hide()
-f.RevertButton = cancelButton
-
-cancelButton:SetScript("OnClick", function()
-	editBox:SetText(db[SELECTION])
-end)
-
-------------------------------------------------------------------------
-
-local reloadButton = CreateFrame("Button", "$parentReloadButton", f, "MagicButtonTemplate")
-reloadButton:SetPoint("BOTTOMLEFT", 7, 5)
-reloadButton:SetText(L.RELOAD)
-reloadButton.LeftSeparator:Hide()
-f.ReloadButton = reloadButton
-
-reloadButton:SetScript("OnClick", function()
-	db[SELECTION] = editBox:GetText()
-	ReloadUI()
-end)
-
-------------------------------------------------------------------------
-
-if LSM and LibStub("PhanxConfig-Dropdown", true) then
-	local font = LibStub("PhanxConfig-Dropdown"):New(f, "Font", nil, LSM:List("font"))
-	font:SetPoint("TOPRIGHT", -10, -15)
-	font:SetWidth(200)
-	f.fontMenu = font
-
-	font.labelText:ClearAllPoints()
-	font.labelText:SetPoint("BOTTOMRIGHT", font, "BOTTOMLEFT", -5, 5)
-
-	function font:OnValueChanged(value, text)
-		local file = LSM:Fetch("font", value)
-		local _, size, flag = self.valueText:GetFont()
-		self.valueText:SetFont(file, size, flag)
-
-		CodeRunnerFont = value
-		editBox:SetFont(file, FONT_SIZE, "")
-	end
-
-	function font:OnListButtonChanged(button, value, selected)
-		if button:IsShown() then
-			button:GetFontString():SetFont(LSM:Fetch("font", value), UIDROPDOWNMENU_DEFAULT_TEXT_HEIGHT)
+	drag:RegisterForDrag("LeftButton")
+	drag:SetScript("OnDragStart", function(self)
+		f:StartMoving()
+		if GameTooltip:IsOwned(self) then
+			GameTooltip:Hide()
 		end
+	end)
+	drag:SetScript("OnDragStop", function(self)
+		f:StopMovingOrSizing()
+		f.userPlaced = true
+		if self:IsMouseOver() then
+			self:GetScript("OnEnter")(self)
+		end
+	end)
+	drag:SetScript("OnHide", function(self)
+		f:StopMovingOrSizing()
+	end)
+
+	drag:RegisterForClicks("RightButtonUp")
+	drag:SetScript("OnClick", function(self, button)
+		if button == "RightButton" then
+			f.userPlaced = false
+			f:Hide()
+			ShowUIPanel(f)
+		end
+	end)
+
+	------------------------------------------------------------------------
+
+	local scrollFrame = CreateFrame("ScrollFrame", "$parentScrollFrame", f)
+	scrollFrame:SetPoint("TOPLEFT", 8, -64)
+	scrollFrame:SetPoint("BOTTOMRIGHT", -32, 29)
+	SCROLL_FRAME_HEIGHT = scrollFrame:GetHeight()
+	f.ScrollFrame = scrollFrame
+
+	local scrollBar = CreateFrame("Slider", "$parentScrollBar", scrollFrame, "UIPanelScrollBarTemplate")
+	scrollBar:SetPoint("TOPLEFT", scrollFrame, "TOPRIGHT", 6, -16)
+	scrollBar:SetPoint("BOTTOMLEFT", scrollFrame, "BOTTOMRIGHT", 6, 16)
+	scrollBar:SetScript("OnValueChanged", nil) -- remove default
+	scrollBar:SetMinMaxValues(0, 1)
+	scrollFrame.ScrollBar = scrollBar
+
+	local scrollBarBG = scrollBar:CreateTexture(nil, "BACKGROUND")
+	scrollBarBG:SetAllPoints(true)
+	scrollBarBG:SetTexture(0,0,0,0.5)
+
+	------------------------------------------------------------------------
+
+	local focus
+
+	local editBox = CreateFrame("EditBox", nil, scrollFrame)
+	scrollFrame:SetScrollChild(editBox)
+	editBox:SetPoint("TOP")
+	editBox:SetPoint("LEFT")
+	editBox:SetPoint("RIGHT")
+	editBox:SetHeight(1000)
+	editBox:SetFontObject(GameFontHighlight)
+	editBox:SetFont(GetFontFile(), FONT_SIZE)
+	editBox:SetTextColor(FONT_R, FONT_G, FONT_B)
+	editBox:SetTextInsets(5,5,5,5)
+	editBox:SetAutoFocus(false)
+	editBox:SetMultiLine(true)
+	f.EditBox = editBox
+
+	editBox:SetScript("OnEscapePressed", editBox.ClearFocus)
+	editBox:SetScript("OnTabPressed", function(self) self:Insert("   ") end)
+	editBox:SetScript("OnEditFocusGained", function(self) focus:Hide() end)
+	editBox:SetScript("OnEditFocusLost", function(self)
+		db[SELECTION] = self:GetText()
+		focus:Show()
+	end)
+	editBox:SetScript("OnShow", function(self)
+		f.TitleText:SetFormattedText("%s - %s", ADDON, SELECTION or UNKNOWN)
+		self:SetText(db[SELECTION] or "")
+		self:SetFocus()
+	end)
+	--[[
+	local editBG = editBox:CreateTexture(nil, "BACKGROUND")
+	editBG:SetAllPoints(true)
+	editBG:SetTexture(0,1,0,0.1)
+	]]
+	focus = CreateFrame("Button", nil, scrollFrame)
+	focus:SetAllPoints(true)
+	focus:SetScript("OnClick", function(self) editBox:SetFocus() end)
+	f.Focus = focus
+
+	------------------------------------------------------------------------
+
+	local function ScrollTo(v)
+		offset = max(min(v, 0), maxOffset)
+		scrollFrame:SetVerticalScroll(-offset)
+		editBox:SetPoint("TOP", 0, offset)
+
+		local perc = maxOffset == 0 and 0 or offset / maxOffset
+		scrollBar:SetValue(perc)
+		scrollBar.ScrollUpButton:SetEnabled(perc > 0.01)
+		scrollBar.ScrollDownButton:SetEnabled(perc < 0.99)
 	end
 
-	font.__SetValue = font.SetValue
-	function font:SetValue(value)
-		self.valueText:SetFont(LSM:Fetch("font", value), FONT_SIZE, "")
-		self:__SetValue(value)
+	editBox:SetScript("OnCursorChanged", function(self, x, y, width, height)
+		LINE_HEIGHT = height
+		if offset < y then
+			ScrollTo(y)
+		elseif floor(offset - SCROLL_FRAME_HEIGHT + height * 2) > y then
+			local v = y + SCROLL_FRAME_HEIGHT - height * 2
+			maxOffset = min(maxOffset, v)
+			ScrollTo(v)
+		end
+	end)
+
+	scrollFrame:UpdateScrollChildRect()
+
+	scrollFrame:EnableMouseWheel(true)
+	scrollFrame:SetScript("OnMouseWheel", function(self, delta)
+		ScrollTo(offset + delta * LINE_HEIGHT * 3)
+	end)
+
+	scrollBar:EnableMouseWheel(true)
+	scrollBar:SetScript("OnMouseWheel", function(self, delta)
+		ScrollTo(offset + delta * LINE_HEIGHT * 3)
+	end)
+
+	scrollBar.ScrollUpButton:SetScript("OnClick", function()
+		ScrollTo(offset + 1 * LINE_HEIGHT * 3)
+	end)
+	scrollBar.ScrollDownButton:SetScript("OnClick", function()
+		ScrollTo(offset - 1 * LINE_HEIGHT * 3)
+	end)
+
+	-- #TODO fix scrollbar with small editbox heights
+
+	------------------------------------------------------------------------
+
+	local runButton = CreateFrame("Button", "$parentRunButton", f, "MagicButtonTemplate")
+	runButton:SetPoint("BOTTOMRIGHT", -7, 5)
+	runButton:SetText(L.RUN)
+	runButton.RightSeparator:Hide()
+	f.RunButton = runButton
+
+	runButton:SetScript("OnClick", function()
+		RunScript(editBox:GetText())
+	end)
+
+	------------------------------------------------------------------------
+
+	local cancelButton = CreateFrame("Button", "$parentRevertButton", f, "MagicButtonTemplate")
+	cancelButton:SetPoint("RIGHT", runButton, "LEFT")
+	cancelButton:SetText(L.REVERT)
+	cancelButton.RightSeparator:Hide()
+	f.RevertButton = cancelButton
+
+	cancelButton:SetScript("OnClick", function()
+		editBox:SetText(db[SELECTION])
+	end)
+
+	------------------------------------------------------------------------
+
+	local reloadButton = CreateFrame("Button", "$parentReloadButton", f, "MagicButtonTemplate")
+	reloadButton:SetPoint("BOTTOMLEFT", 7, 5)
+	reloadButton:SetText(L.RELOAD)
+	reloadButton.LeftSeparator:Hide()
+	f.ReloadButton = reloadButton
+
+	reloadButton:SetScript("OnClick", function()
+		db[SELECTION] = editBox:GetText()
+		ReloadUI()
+	end)
+
+	------------------------------------------------------------------------
+
+	if LSM and LibStub("PhanxConfig-Dropdown", true) then
+		local font = LibStub("PhanxConfig-Dropdown"):New(f, "Font", nil, LSM:List("font"))
+		font:SetPoint("TOPRIGHT", -10, -15)
+		font:SetWidth(200)
+		f.fontMenu = font
+
+		font.labelText:ClearAllPoints()
+		font.labelText:SetPoint("BOTTOMRIGHT", font, "BOTTOMLEFT", -5, 5)
+
+		function font:OnValueChanged(value, text)
+			local file = LSM:Fetch("font", value)
+			local _, size, flag = self.valueText:GetFont()
+			self.valueText:SetFont(file, size, flag)
+
+			CodeRunnerFont = value
+			editBox:SetFont(file, FONT_SIZE, "")
+		end
+
+		function font:OnListButtonChanged(button, value, selected)
+			if button:IsShown() then
+				button:GetFontString():SetFont(LSM:Fetch("font", value), UIDROPDOWNMENU_DEFAULT_TEXT_HEIGHT)
+			end
+		end
+
+		font.__SetValue = font.SetValue
+		function font:SetValue(value)
+			self.valueText:SetFont(LSM:Fetch("font", value), FONT_SIZE, "")
+			self:__SetValue(value)
+		end
+		
+		font:SetValue(CodeRunnerFont)
 	end
-end
+
+	------------------------------------------------------------------------
+
+end) -- f:OnShow
 
 ------------------------------------------------------------------------
 
@@ -341,10 +351,6 @@ f:SetScript("OnEvent", function(self, event, addon)
 		CodeRunnerSelection = SELECTION
 
 		CodeRunnerFont = CodeRunnerFont or "Inconsolata"
-		editBox:SetFont(GetFontFile(), FONT_SIZE)
-		if self.fontMenu then
-			self.fontMenu:SetValue(CodeRunnerFont)
-		end
 
 		if not db[SELECTION] then
 			db[SELECTION] = ""
